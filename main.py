bb52e51f69dcd36c9d7f9c160d53de74d3c90127
evil_rise7 <bakha898@mail.ru>
2019-03-11 18:16:51 +0600

    Add news

--- main.py
+++ main.py
@@ -1,25 +1,91 @@
-from flask import Flask, request, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired
+from flask import Flask, render_template, redirect, request, session
+# import requests
+import json
+from news_model import *
+from db import *
+from users_model import *
 
-
+db = DB()
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
         if request.method == 'GET':
-            return render_template('index.html')
+            return render_template('login.html')
         elif request.method == 'POST':
             print(request.form['email'])
             print(request.form['password'])
+            session['username'] = request.form['email']
             return redirect('/main')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', 0)
+    return redirect('/login')
+
+
+class AddNewsForm(FlaskForm):
+    title = StringField('Заголовок новости', validators=[DataRequired()])
+    content = TextAreaField('Текст новости', validators=[DataRequired()])
+    submit = SubmitField('Добавить')
+
+
+# @app.route('/add_news', methods=['GET', 'POST'])
+# def add_news():
+#     if 'username' not in session:
+#         return redirect('/login')
+#     # form = AddNewsForm()
+#
+#     print("got to add_news")
+#     if request.method == "POST":
+#         title = request.form["comment"]
+#         content = request.form["uploading_files"]
+#         print("got here")
+#         nm = NewsModel(db.get_connection())
+#
+#         nm.insert(title, content, session['username'])  # CHANGE CHANGE to user id
+#         return redirect("/index")
+#     else:
+#         return render_template('add_news.html', title='Добавление новости', username=session['username'])
+
+
+@app.route('/delete_news/<int:news_id>', methods=['GET'])
+def delete_news(news_id):
+    if 'username' not in session:
+        return redirect('/login')
+    nm = NewsModel(db.get_connection())
+    nm.delete(news_id)
+    return redirect("/index")
 
 
 @app.route('/main', methods=['POST', 'GET'])
 def main():
-    if request.method == 'GET':
-        return render_template('home.html')
+    if 'username' not in session:
+        return redirect('/login')
+    # form = AddNewsForm()
 
+    print("got to add_news")
+    if request.method == "POST":
+        print("got here")
+        content = request.form["comment"]
+        # content = request.files["uploadingfiles"]
+        print("pass here")
+        nm = NewsModel(db.get_connection())
+        nm.init_table()
+        print(session['username'])
+        nm.insert(content, content, session['username'])  # CHANGE CHANGE to user id
+
+        print(nm.get_all())
+        return redirect("/main")
+    else:
+        return render_template('home.html', title='Добавление новости', username=session['username'])
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    app.run(port=8082, host='127.0.0.1')
---
+++ news_model.py
@@ -0,0 +1,47 @@
+class NewsModel:
+    def __init__(self, connection):
+        self.connection = connection
+
+    def init_table(self):
+        cursor = self.connection.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS news 
+                                (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                                 title VARCHAR(100),
+                                 content VARCHAR(1000),
+                                 user_id INTEGER,
+                                 likes INTEGER
+                                 )''')
+        cursor.close()
+        self.connection.commit()
+
+    def insert(self, title, content, user_id):
+        cursor = self.connection.cursor()
+        cursor.execute('''INSERT INTO news 
+                          (title, content, user_id) 
+                          VALUES (?,?,?)''', (title, content, str(user_id)))
+        cursor.close()
+        self.connection.commit()
+
+    def get(self, news_id):
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM news WHERE id = ?", (str(news_id),))
+        row = cursor.fetchone()
+        return row
+
+    def get_all(self, user_id=None):
+        cursor = self.connection.cursor()
+        if user_id:
+            cursor.execute("SELECT * FROM news WHERE user_id = ?", (str(user_id),))
+        else:
+            cursor.execute("SELECT * FROM news")
+        rows = cursor.fetchall()
+        return rows
+
+    def get_likes(self, news_id):
+        pass
+
+    def delete(self, news_id):
+        cursor = self.connection.cursor()
+        cursor.execute('''DELETE FROM news WHERE id = ?''', (str(news_id),))
+        cursor.close()
+        self.connection.commit()
